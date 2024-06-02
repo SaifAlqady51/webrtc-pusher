dda355b11a4136e67330f053908a3c9ad90145a9
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-02 17:52:00 +0300

    feat: Updated useMediaStream hook

--- src/hooks/useMediaStream.tsx
+++ src/hooks/useMediaStream.tsx
@@ -1,29 +1,27 @@
-import {
-  useState,
-  useCallback,
-  MutableRefObject,
-  RefObject,
-  Dispatch,
-  SetStateAction,
-} from "react";
+import { PresenceChannel } from "pusher-js";
+import { Dispatch, MutableRefObject, RefObject, SetStateAction } from "react";
 
-type MediaStreamHookProps = {
-  userStream: MutableRefObject<MediaStream | undefined>;
+type StreamHookProps = {
+  micActive: boolean;
+  cameraActive: boolean;
+  setMicActive: Dispatch<SetStateAction<boolean>>;
+  setCameraActive: Dispatch<SetStateAction<boolean>>;
+  userStream: MutableRefObject<MediaStream | null>;
   userVideo: RefObject<HTMLVideoElement>;
-  isMicActive: boolean;
-  isCameraActive: boolean;
-  setIsMicActive: Dispatch<SetStateAction<boolean>>;
-  setIsCameraActive: Dispatch<SetStateAction<boolean>>;
+  host: MutableRefObject<boolean>;
+  channelRef: MutableRefObject<PresenceChannel | null>;
 };
 
-export const useMediaStream = ({
+export function useStreamHook({
+  micActive,
+  cameraActive,
   userStream,
+  setMicActive,
+  setCameraActive,
   userVideo,
-  isMicActive,
-  isCameraActive,
-  setIsMicActive,
-  setIsCameraActive,
-}: MediaStreamHookProps) => {
+  host,
+  channelRef,
+}: StreamHookProps) {
   const handleRoomJoined = () => {
     navigator.mediaDevices
       .getUserMedia({
@@ -31,17 +29,46 @@ export const useMediaStream = ({
         video: { width: 1280, height: 720 },
       })
       .then((stream) => {
+        /* use the stream */
         userStream.current = stream;
         userVideo.current!.srcObject = stream;
         userVideo.current!.onloadedmetadata = () => {
           userVideo.current!.play();
         };
+        if (!host.current) {
+          // the 2nd peer joining will tell to host they are ready
+          console.log("triggering client ready");
+          channelRef.current!.trigger("client-ready", {});
+        }
       })
       .catch((err) => {
-        handleMediaError(err);
+        /* handle the error */
+        console.error("Error accessing media devices:", err);
+        if (err.name === "NotAllowedError") {
+          alert(
+            "Permission to access camera and microphone was denied. Please allow access."
+          );
+        } else if (
+          err.name === "NotFoundError" ||
+          err.name === "DevicesNotFoundError"
+        ) {
+          alert(
+            "No camera or microphone found. Please connect a camera and microphone."
+          );
+        } else if (
+          err.name === "NotReadableError" ||
+          err.name === "TrackStartError"
+        ) {
+          alert(
+            "Camera or microphone is already in use by another application."
+          );
+        } else {
+          alert(
+            "An error occurred while accessing media devices. Please check your settings and try again."
+          );
+        }
       });
   };
-
   const toggleMediaStream = (type: "video" | "audio", state: boolean) => {
     userStream.current!.getTracks().forEach((track) => {
       if (track.kind === type) {
@@ -51,41 +78,20 @@ export const useMediaStream = ({
   };
 
   const toggleMic = () => {
-    toggleMediaStream("audio", isMicActive);
-    setIsMicActive((prev) => !prev);
+    toggleMediaStream("audio", micActive);
+
+    setMicActive((prev) => !prev);
   };
+
   const toggleCamera = () => {
-    toggleMediaStream("video", isCameraActive);
-    setIsCameraActive((prev) => !prev);
+    toggleMediaStream("video", cameraActive);
+
+    setCameraActive((prev) => !prev);
   };
+
   return {
-    handleRoomJoined,
     toggleMic,
     toggleCamera,
+    handleRoomJoined,
   };
-};
-
-const handleMediaError = (err: Error) => {
-  console.error("Error accessing media devices:", err);
-  if (err.name === "NotAllowedError") {
-    alert(
-      "Permission to access camera and microphone was denied. Please allow access."
-    );
-  } else if (
-    err.name === "NotFoundError" ||
-    err.name === "DevicesNotFoundError"
-  ) {
-    alert(
-      "No camera or microphone found. Please connect a camera and microphone."
-    );
-  } else if (
-    err.name === "NotReadableError" ||
-    err.name === "TrackStartError"
-  ) {
-    alert("Camera or microphone is already in use by another application.");
-  } else {
-    alert(
-      "An error occurred while accessing media devices. Please check your settings and try again."
-    );
-  }
-};
+}
