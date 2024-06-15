2744928067c2b0f8b49044a185ab3f422e7d5c04
SaifAlqady51 <saifalqady51@gmail.com>
2024-06-15 18:46:51 +0300

    feat: Created Home page

--- src/app/room/page.tsx
+++ src/app/room/page.tsx
@@ -1,13 +1,10 @@
 import Room from "@/components/Room";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1>Home</h1>
-      <Link href="/room" className="py-2 px-4 font-semibold text-3xl">
-        Join Room
-      </Link>
+      <Room />
     </div>
   );
 }
